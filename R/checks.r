5acc2f8071d1209b9199b9eda874898c3ec0bff4
kaneplusplus <kaneplusplus@gmail.com>
2020-05-25 19:48:49 -0400

    Initial commit.

---
+++ R/checks.r
@@ -0,0 +1,25 @@
+
+check_only_one_dependent_var <- function(x) {
+  if (sum(x$role == "dependent") > 1) {
+    stop(red("You may only have one dependent variable."))
+  }
+}
+
+check_at_least_one_indep_var <- function(x) {
+  if (sum(x$role == "independent") == 0) {
+    stop(red("You must have at least one independent variable."))
+  }
+}
+
+conditional_not_yet_supported <- function(x) {
+  if (sum(x$role == "conditional") > 0) {
+    stop(red("Conditional variables are not currently supported."))
+  }
+}
+
+check_dependent <- function(x, dependent_types) {
+  if (!any(x$class[x$role == "dependent"] %in% dependent_types)) {
+    stop(red("Unsupported dependent variable type:",
+             class(x$class[var_desc$role == "dependent"])))
+  }
+}
